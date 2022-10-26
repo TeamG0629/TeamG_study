f2ca6c19e4ad05fb2c4629e77a8fddcb53729860
bashi <ykh2135238@stu.o-hara.ac.jp>
2022-10-26 14:17:14 +0900

    10/26 髙橋

--- precomi/models.py
+++ precomi/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+
+#プロフィールモデル
+class User(models.Model):
+
--- precomi/views.py
+++ precomi/views.py
@@ -2,6 +2,58 @@ from django.shortcuts import render
 from django.views import generic
 
 
-#インデックスhtmlに飛ばす
+#index.htmlに飛ばす
 class IndexView(generic.TemplateView):
     template_name = "index.html"
+
+
+#talk.htmlに飛ばす
+class TalkView(generic.TemplateView):
+    template_name = "talk.html"
+
+
+
+#プロフィール===============================================
+#profile.htmlに飛ばす
+class ProfileView(generic.TemplateView):
+    template_name = "profile.html"
+
+
+#profile作成
+class ProfileCreateView(generic.CreateView):
+    model = User
+    template_name = "profile_create.html"
+
+
+#profileアップデート
+class ProfileUpdateView(generic.UpdateView):
+    model = User
+    template_name = "profile_update.html"
+#======================================================
+
+
+#日記===================================================
+#diary.htmlに飛ばす
+class DiaryView(generic.TemplateView):
+    template_name = "diary.html"
+
+
+#diary作成
+class DiaryCreateView(generic.CreateView):
+    model = Diary
+    template_name = "diary_create.html"
+
+
+#diaryアップデート
+class DiaryUpdateView(generic.UpdateView):
+    model = Diary
+    template_name = "diary_update.html"
+
+
+#diary削除
+class DiaryDeleteView(generic.DeleteView):
+    model = Diary
+    template_name = "diary_delete.html"
+#======================================================
+
+
