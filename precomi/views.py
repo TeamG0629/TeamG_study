119c0b0f6510951f358852730c5da5adaf38004f
bashi <ykh2135238@stu.o-hara.ac.jp>
2022-10-28 11:36:06 +0900

    10/28 モデル完成(戻し) 髙橋

--- precomi/views.py
+++ precomi/views.py
@@ -7,53 +7,53 @@ class IndexView(generic.TemplateView):
     template_name = "index.html"
 
 
-#talk.htmlに飛ばす
-class TalkView(generic.TemplateView):
-    template_name = "talk.html"
-
-
-
-#プロフィール===============================================
-#profile.htmlに飛ばす
-class ProfileView(generic.TemplateView):
-    template_name = "profile.html"
-
-
-#profile作成
-class ProfileCreateView(generic.CreateView):
-    model = User_info
-    template_name = "profile_create.html"
-
-
-#profileアップデート
-class ProfileUpdateView(generic.UpdateView):
-    model = User_info
-    template_name = "profile_update.html"
-#======================================================
-
-
-#日記===================================================
-#diary.htmlに飛ばす
-class DiaryView(generic.TemplateView):
-    template_name = "diary.html"
-
-
-#diary作成
-class DiaryCreateView(generic.CreateView):
-    model = Diary
-    template_name = "diary_create.html"
-
-
-#diaryアップデート
-class DiaryUpdateView(generic.UpdateView):
-    model = Diary
-    template_name = "diary_update.html"
-
-
-#diary削除
-class DiaryDeleteView(generic.DeleteView):
-    model = Diary
-    template_name = "diary_delete.html"
-#======================================================
+# #talk.htmlに飛ばす
+# class TalkView(generic.TemplateView):
+#     template_name = "talk.html"
+#
+#
+#
+# #プロフィール===============================================
+# #profile.htmlに飛ばす
+# class ProfileView(generic.TemplateView):
+#     template_name = "profile.html"
+#
+#
+# #profile作成
+# class ProfileCreateView(generic.CreateView):
+#     model = User_info
+#     template_name = "profile_create.html"
+#
+#
+# #profileアップデート
+# class ProfileUpdateView(generic.UpdateView):
+#     model = User_info
+#     template_name = "profile_update.html"
+# #======================================================
+#
+#
+# #日記===================================================
+# #diary.htmlに飛ばす
+# class DiaryView(generic.TemplateView):
+#     template_name = "diary.html"
+#
+#
+# #diary作成
+# class DiaryCreateView(generic.CreateView):
+#     model = Diary
+#     template_name = "diary_create.html"
+#
+#
+# #diaryアップデート
+# class DiaryUpdateView(generic.UpdateView):
+#     model = Diary
+#     template_name = "diary_update.html"
+#
+#
+# #diary削除
+# class DiaryDeleteView(generic.DeleteView):
+#     model = Diary
+#     template_name = "diary_delete.html"
+# #======================================================
 
 
