8103753b01f6e5764c2390e9e243f0ecdf34c42e
bashi <ykh2135238@stu.o-hara.ac.jp>
2022-11-07 15:54:11 +0900

    11/07 新規作成 髙橋

---
+++ teamG_study/precomi/forms.py
@@ -0,0 +1,14 @@
+import os
+from django import forms
+from .models import User
+
+
+#ユーザプロフィールの新規作成(誕生日、性別、電話番号、妊娠周期、血液型、症状)
+class UserCreateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('datebirth','sex','telnum','precycle','bloodtype','symptoms')
+    def __int__(self,args,**kwargs):
+        super().__init__(*args,**kwargs)
+        for fields in self.fields.values():
+            fields.widget.attrs['class'] = 'form-control'
--- teamG_study/precomi/views.py
+++ teamG_study/precomi/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views import generic
-
+from .forms import UserCreateForm
+from  django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
+from .models import User
+from django.contrib import messages
 
 #index.htmlに飛ばす
 class IndexView(generic.TemplateView):
@@ -10,14 +13,37 @@ class IndexView(generic.TemplateView):
 class TalkView(generic.TemplateView):
     template_name = "talk.html"
 
+#profile.htmlに飛ばす
 class ProfileView(generic.TemplateView):
     template_name = "profile.html"
 
-class EditView(generic.TemplateView):
-    template_name = "edit.html"
+#profile_update.htmlに飛ばす
+class ProfileUpdateView(generic.TemplateView):
+    template_name = "profile_update.html"
 
+#緊急通知の画面に飛ばす
 class NoticeView(generic.TemplateView):
     template_name = "notice.html"
+
+#profile_create.htmlに飛ばす(新規作成)
+class ProfileCreateView(LoginRequiredMixin, generic.CreateView):
+    model = User
+    template_name = 'profile_create.html'
+    form_class = UserCreateForm
+
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        user.user = self.request.user
+        user.save()
+        messages.success(self.request, '作成しました')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, "作成に失敗しました")
+        return super().form_invalid(form)
+
+
+
 
 # #プロフィール===============================================
 # #profile.htmlに飛ばす
