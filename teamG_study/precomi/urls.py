d341acb142e9b4aafdef9623195006bedff75783
kdmyte103 <ykh2135223@stu.o-hara.ac.jp>
2022-11-08 14:49:41 +0900

    11/8 14:49 宮田

--- teamG_study/precomi/urls.py
+++ teamG_study/precomi/urls.py
@@ -9,10 +9,11 @@ urlpatterns = [
     path('profile_update/',views.ProfileUpdateView.as_view(),name="profile_update"),
     path('profile_create/',views.ProfileCreateView.as_view(),name="profile_create"),
     path('notice/',views.NoticeView.as_view(),name="notice"),
+    path('diary/', views.DiaryView.as_view(), name="diary"),
     # path('profile-create',views.ProfileCreateView(),name="profile_create"),
     # path('profile-update',views.ProfileUpdateView(),name="profile_update"),
-    # path('diary/',views.DiaryView(),name="diary"),
     # path('diary-create',views.DiaryCreateView(),name="diary_create"),
     # path('diary-update',views.DiaryUpdateView(),name="diary_update"),
     # path('diary-delete',views.DiaryDeleteView(),name="diary_delete"),
 ]
+
